2d315a8fcb6011b276ed8951d44fc42410836e66
elliothimmelfarb <elliothimmelfarb@gmail.com>
2016-06-16 19:52:04 -0700

    CRUD

--- app.js
+++ app.js
@@ -23,6 +23,44 @@ app.get('/', (req, res) => {
   res.send('hello word!');
 })
 
+app.get('/msgboard', (req, res) => {
+  message.get((err, msgs) => {
+    if(err) return res.status(400).send(err);
+    res.send(msgs);
+  });
+});
+
+app.get('/msgboard')
+
+app.post('/msgboard', (req, res) => {
+  message.create(req.body.author, req.body.text, (err) => {
+    if(err) return res.status(400).send(err);
+    res.send('Posted!');
+  });
+});
+
+app.delete('/msgboard/:id', (req, res) => {
+  message.delete(req.params.id, err => {
+    if(err) return res.status(400).send(err);
+    res.send('Deleted!');
+  });
+});
+
+app.put('/msgboard', (req, res) => {
+  message.edit(req.body.id, req.body.text, err => {
+    if(err) return res.status(400).send(err);
+    res.send('Edited!');
+  });
+});
+
+
+
+app.post('/msgs', (req, rest) => {});
+app.delete('/msgs', (req, rest) => {});
+app.put('/msgs', (req, rest) => {});
+
+
+
 /////////// APP LISTEN ///////////
 app.listen(PORT, err => {
   console.log(err || `Express listening on port ${PORT}`);
--- message.js
+++ message.js
@@ -1,3 +1,69 @@
 'use strict';
 
-function readMsgs()
+const fs = require('fs');
+const path = require('path');
+const uuid = require('uuid');
+const moment = require('moment');
+
+const dataPath = path.join(__dirname, 'messages.json');
+
+exports.get = cb => {
+  readMsgs(cb);
+}
+
+exports.create = (author, text, cb) => {
+  readMsgs((err, msgs) => {
+    let msgObj = {};
+    msgObj.author = author;
+    msgObj.text = text;
+    msgObj.time = moment().format('MMMM Do YYYY, h:mm:ss a');
+    msgObj.id = uuid();
+    msgs.push(msgObj);
+    writeMsgs(msgs, cb);
+  });
+};
+
+exports.delete = (id, cb) => {
+  readMsgs((err, msgs) => {
+    for(let i = 0; i < msgs.length; i++) {
+      if(msgs[i].id === id) {
+        msgs.splice(i, 1);
+        break;
+      }
+    }
+    writeMsgs(msgs, cb);
+  });
+};
+
+exports.edit = (id, text, cb) => {
+  readMsgs((err, msgs) => {
+    let index, msgObj;
+    for(let i = 0; i < msgs.length; i++) {
+      if(msgs[i].id === id) {
+        index = i;
+        msgObj = msgs[i];
+        break;
+      }
+    }
+    msgObj.text = text;
+    msgObj.edited = moment().format('MMMM Do YYYY, h:mm:ss a');
+    msgs[index] = msgObj;
+    writeMsgs(msgs, cb);
+  });
+};
+
+function writeMsgs(msgs, cb) {
+  fs.writeFile(dataPath, JSON.stringify(msgs), cb);
+}
+
+function readMsgs(cb) {
+  fs.readFile(dataPath, (err, data) => {
+    if (err) return cb(err);
+    try {
+      var msgs = JSON.parse(data);
+    } catch (e) {
+      var msgs = [];
+    }
+    cb(null, msgs);
+  });
+}
